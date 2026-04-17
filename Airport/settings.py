bf7ec90cad971c5c0e142cb210a97483fad79725
Ruslan <stadnikruslan168@gmail.com>
2026-04-17 18:09:13 +0300

    Change settings project name

--- Airport/settings.py
+++ Airport/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Airport_app',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoProject.urls'
+ROOT_URLCONF = 'Airport.urls'
 
 TEMPLATES = [
     {
